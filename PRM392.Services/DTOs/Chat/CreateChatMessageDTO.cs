e147d650f8af4b4680d82b3cba5d5618eaae4afb
phucpu03fptvn <156656593+phucpu03fptvn@users.noreply.github.com>
2025-03-27 16:08:20 +0700

    [PhucNHSE][ChatMessage and Notification functions]

---
+++ PRM392.Services/DTOs/Chat/CreateChatMessageDTO.cs
@@ -0,0 +1,15 @@
+﻿using PRM392.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRM392.Services.DTOs.Chat
+{
+    public class CreateChatMessageDTO
+    {
+        public string? Message { get; set; }
+        public string? UserId { get; set; }
+    }
+}
---
+++ PRM392.Services/Interfaces/IChatMessageService.cs
@@ -0,0 +1,17 @@
+﻿using PRM392.Repositories.Models;
+using PRM392.Services.DTOs.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRM392.Services.Interfaces
+{
+    public interface IChatMessageService
+    {
+        Task<ApplicationResponse> CreateMessageAsync(CreateChatMessageDTO createChatMessageDTO);
+        Task<ApplicationResponse> DeleteMessageAsync(string id);
+        Task<ApplicationResponse> GetMessagesAsync();
+    }
+}
